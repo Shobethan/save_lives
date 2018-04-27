a324d7945ef157766fa8bc7ea9a179a21ebb4722
Shobethan <mshobethan@gmail.com>
2018-04-27 22:01:45 +0530

    TabsPage Updated plus small updates

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { NeedsPage } from '../needs/needs';
+import { EventsPage } from '../events/events';
+import { ProfilePage } from '../profile/profile';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = NeedsPage;
+  tab2Root = EventsPage;
+  tab3Root = ProfilePage;
+  tab4Root = SettingsPage;
 
   constructor() {
 
